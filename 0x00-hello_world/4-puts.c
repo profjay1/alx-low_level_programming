d85b7d6ea070db1bfdaf7d0378df9c5d9559426e
Profjay <jibstechnology@gmail.com>
2022-06-30 14:57:05 -0700

    2 to 6 committed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ *
+ *  * main - init
+ *
+ *   * Return: 0
+ *
+ *   */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
-	
+	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ *
+ *  * main - init
+ *
+ *   * Return: 0
+ *
+ *   */
+
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,");
-
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
