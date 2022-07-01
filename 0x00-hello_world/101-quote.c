3f331fe77b19d8937ae8586789770cf83c1003ba
Profjay <jibstechnology@gmail.com>
2022-06-30 17:49:54 -0700

    101 committed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,7 @@
 
 int main(void)
 {
-
-	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	fputs("and that piece of art is useful\" - \Dora Korpar, 2015-10-19\n", stdout);
 
 	return (1);
 
