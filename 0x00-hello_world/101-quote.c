b8cc9adc852d42e7de0d26320f4ef1e6d95e33cb
Profjay <jibstechnology@gmail.com>
2022-06-30 23:22:03 -0700

    101 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar,\n
-		       
-	2015-10-19\n", 59);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return (0);
+	return (1);
 }
