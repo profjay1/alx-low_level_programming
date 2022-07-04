e9ea9d2c435ad9671301d60b9c7b9c82868ae4cc
Profjay <jibstechnology@gmail.com>
2022-07-04 16:25:10 -0700

    101 commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a<= 9; a++)
+	{
+		for(b = a + 1; b <= 9; b++)
+		{
+			if (a = 8 && b = 9)
+			{
+				printf("%d%d", a, b);
+			}
+			else
+			{
+				printf("%d%d, ", a, b)
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
