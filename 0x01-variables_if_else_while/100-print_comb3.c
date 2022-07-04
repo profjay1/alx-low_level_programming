ed38ab7d22505bbaf99465d7bd4f64d64c41d960
Profjay <jibstechnology@gmail.com>
2022-07-04 16:40:35 -0700

    100 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -11,9 +11,9 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a<= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for(b = a + 1; b <= 9; b++)
+		for (b = a + 1; b <= 9; b++)
 		{
 			if (a == 8 && b == 9)
 			{
