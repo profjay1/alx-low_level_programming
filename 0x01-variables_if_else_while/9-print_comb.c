556fcd014dca18e87a8e2f56f34fbc93e7d25527
Profjay <jibstechnology@gmail.com>
2022-07-04 13:37:54 -0700

    Task 9 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always ) (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+
+		if (num != '9')
+		{
+			putchar(',');
+
+			putchar(' ');
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+	
+}
