d483306030a2f95180aa0ae0d9c64921d0a9f0f9
Profjay <jibstechnology@gmail.com>
2022-07-03 17:41:45 -0700

    4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;  
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		while (ch == e || ch == q)
+		{
+			continue;
+		}
+
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
