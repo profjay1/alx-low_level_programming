ccfa71f9062a56e452fbf517d2b1534a5e1d5820
Profjay <jibstechnology@gmail.com>
2022-07-04 18:29:54 -0700

    102 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,27 +8,30 @@
 
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+	int i;
+	int j;
 
-	for (a = 0; a <= 9; a++)
+	for (i = 0; i < 100; i++)
 	{
-		for (b = a; b <= 9; b++)
+		for (j = 0; j < 100; j++)
 		{
-			for (c = b; c <= 9; c++)
+			if (i < j)
 			{
-				for (d = c + 1; d <= 9; d++)
+				putchar((i / 10) + 48);
+
+				putchar((i % 10) + 48);
+
+				putchar(' ');
+
+				putchar((j / 10) + 48);
+
+				putchar((j % 10) + 48);
+
+				if (i != 98 || j != 99)
 				{
-					if (a == 9 && b == 8 && c == 9 && d == 9)
-					{
-						printf("%d%d %d%d", a, b, c, d);
-					}
-					else
-					{
-						printf("%d%d %d%d, ", a, b, c, d);
-					}
+					putchar(',');
+
+					putchar(' ');
 				}
 			}
 		}
