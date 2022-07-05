02bf96e69702b72e4b77d46e010e21bf85b1d5e3
Profjay <jibstechnology@gmail.com>
2022-07-04 17:02:24 -0700

    101 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- *
+ * main - Entry point
+ *  
  * Return: always 0 (Success)
  */
 
@@ -12,16 +12,18 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = a + 1; b<= 9; b++)
+		for (b = a + 1; b <= 9; b++)
 		{
 			for (c = b + 1; c <= 9; c++)
 			{
 				if (a == 7 && b == 8 && c == 9)
 				 {
+
 				 	printf("%d%d%d", a, b, c);
 			       	 }
 				else
 				{
+
 					printf("%d%d%d, ", a, b, c);
 				}
 			}
@@ -31,5 +33,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-
 }	
