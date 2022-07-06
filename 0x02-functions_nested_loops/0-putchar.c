d6ff394a29e35c72c4a813659f80c3def15ff3ba
Profjay <jibstechnology@gmail.com>
2022-07-06 09:49:00 -0700

    Task 0 commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main"
 
 /**
  * main - Entry point
@@ -8,9 +8,15 @@
 
 int main(void)
 {
-	_putchar('_putchar');
-	
-	_putchar('\n');
+	_putchar('_');
+	_putchar('p'); 
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+ 	_putchar('\n');
 
 	return (0);
 }
