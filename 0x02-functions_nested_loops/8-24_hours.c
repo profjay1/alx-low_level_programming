5ea6ca57d90be5128372f01d07ae9b6669ed694f
Profjay <jibstechnology@gmail.com>
2022-07-13 07:24:19 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,17 +15,17 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			putchar((h / 10) + '0');
+			_putchar((h / 10) + '0');
 
-			putchar((h % 10) + '0');
+			_putchar((h % 10) + '0');
 
-			putchar(':');
+			_putchar(':');
 
-			putchar((m / 10) + '0');
+			_putchar((m / 10) + '0');
 
-			putchar((m % 10) + '0');
+			_putchar((m % 10) + '0');
 
-			putchar('\n');
+			_putchar('\n');
 		}
 
 	}
