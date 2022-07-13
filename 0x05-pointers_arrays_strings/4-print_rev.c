53a5a550693919ef1c4f84086f51582a753a89b7
Profjay <jibstechnology@gmail.com>
2022-07-13 15:59:12 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ * print_rev - prints a string in reverse followded by a new line
+ *
+ * @s: str to be processed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+		for (len = len - 1; len >= 0; len--)
+		{
+			putchar(s[len]);
+		}
+		putchar('\n');
+}
