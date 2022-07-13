43d2c7c9e17437d4ff16d2a85df57af8bf5ab811
Profjay <jibstechnology@gmail.com>
2022-07-13 07:10:55 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of ant integer
+ * @n: integer to be processed
+ * Return: las digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	int a = (n % 10);
+
+	if (a < 0)
+
+	{
+		a = (a * -1);
+
+		_putchar(a + '0');
+
+		return (a);
+	}
+	else
+	{
+		_putchar(a + '0');
+
+		return (a);
+	}
+}
