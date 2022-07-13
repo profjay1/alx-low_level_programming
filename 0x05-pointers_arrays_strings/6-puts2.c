8e95189645ac868a40536ad3517d744e24f9c74b
Profjay <jibstechnology@gmail.com>
2022-07-13 16:29:11 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print pairs
+ *
+ * @str: string to be processed
+ *
+ * Return: void
+ */
+
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if (i % 2 == 0)
+		{
+			putchar(*(str + i));
+		}
+		i++;
+	}
+	putchar('\n');
+}
