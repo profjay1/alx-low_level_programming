914936cd7b292da6c67ca0d24c8ebb45a501a7f0
Profjay <jibstechnology@gmail.com>
2022-07-13 07:21:08 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of a day
+ *
+ * Return: 0
+ */
+
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			putchar((h / 10) + '0');
+
+			putchar((h % 10) + '0');
+
+			putchar(':');
+
+			putchar((m / 10) + '0');
+
+			putchar((m % 10) + '0');
+
+			putchar('\n');
+		}
+
+	}
+}
