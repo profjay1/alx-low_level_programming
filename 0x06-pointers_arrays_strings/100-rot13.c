ea54be35acadc52903fbe97e89135b2d292fab3a
Profjay <jibstechnology@gmail.com>
2022-07-14 16:07:23 -0700

    Task advance 1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13
+ *
+ * @s: string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		
+		while (s1[j] != '\0')
+		{
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
