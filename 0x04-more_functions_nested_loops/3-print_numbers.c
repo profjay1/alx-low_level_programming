87031c3efb8dc720cd20ec73f233d9e897017747
Profjay <jibstechnology@gmail.com>
2022-07-14 10:43:42 -0700

    Printing 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the number from 0 to 9 followed by a new line
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+
+	}
+	putchar('\n');
+}
