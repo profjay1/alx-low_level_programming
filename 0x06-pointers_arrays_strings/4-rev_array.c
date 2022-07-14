0af476ea5985c5a413b9c3d204cfb87f93b6f93e
Profjay <jibstechnology@gmail.com>
2022-07-14 14:58:26 -0700

    Reverse printing

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse a content of an array of integers
+ *
+ * @a: array to be reversed
+ *
+ * @n: number of elements of the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int aux;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		aux = a[i];
+
+		a[i] = a[n - 1];
+
+		a[n - 1] = aux;
+
+		n--;
+	}
+}
