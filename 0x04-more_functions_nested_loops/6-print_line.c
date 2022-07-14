f67d5b44eb9ea49b2f884de862d5007cfed4ec4a
Profjay <jibstechnology@gmail.com>
2022-07-14 16:49:06 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ *
+ * @n: number of line that will be printed
+ *
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	if (n >= 0)
+	{
+		while (n != 0)
+		{
+			putchar('_');
+
+			n--;
+		}
+	}
+	putchar('\n');
+}
