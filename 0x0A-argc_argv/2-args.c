1ec47a06a9eaf768d6751a5579650e09147206a7
Profjay <jibstechnology@gmail.com>
2022-07-23 04:53:47 -0700

    Print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ *
+ * @argc: arguments vector
+ *
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
