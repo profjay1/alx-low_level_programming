4a49755e988c7cfd28be7082a07bf48280b6e991
Profjay <jibstechnology@gmail.com>
2022-08-02 11:43:42 -0700

    Task 4 correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,6 +50,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; owner[i]; i++)
 		cpyowner[i] = owner[i];
 	cpyowner[i] = '\0';
+
 	new_dog->name = cpyname;
 	new_dog->age = age;
 	new_dog->owner = cpyowner;
