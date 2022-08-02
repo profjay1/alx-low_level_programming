5b085c88a94585068a9ad2f6b41d6a9456923769
Profjay <jibstechnology@gmail.com>
2022-08-02 00:13:50 -0700

    Task 4 correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,59 +1,55 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * new_dog - creates a new dog
+ * new_dog - creates a new dog structure
  *
- * @name: name of the dog
+ * @name: Dog's name
  *
- * @age: age of the dog
+ * @age: Dog's age
  *
- * @owner: owner of the dog
+ * @owner: Dog's owner
  *
- * Return: pointer to the new dog
+ * Return: returns a pointer to newly created dog structure
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int i;
+	dog_t *new_dog;
 
-	dog_t *dog;
+	char *cpyname, *cpyowner;
+
+	int len_name = 0, len_owner = 0, i;
 
 	if (name == NULL || owner == NULL)
-	{
 		return (NULL);
-	}
-	dog = malloc(sizeof(dog_t));
-
-	if (!dog)
-	{
+	
+	while (name[len_name])
+		len_name++;
+	
+	while (owner[len_owner])
+		len_owner++;
+		new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
-	}
-	dog->name = malloc(strlen(name) + 1 * sizeof(char));
+	
+	cpyname = malloc(len_name + 1);
 
-	if (dog->name == NULL)
-	{
-		free(dog);
+	if (cpyname == NULL)
 		return (NULL);
-	}
-	for (i = 0; i <= strlen(name); i++)
-	{
-		dog->name[i] = name[i];
-	}
-	dog->age = age;
-	dog->owner = malloc(strlen(owner) + 1 * sizeof(char));
-
-	if (dog->owner == NULL)
-	{
-		free(dog->name);
-
-		free(dog);
+	
+	for (i = 0; name[i]; i++)
+		cpyname[i] = name[i];
+		cpyname[i] = '\0';
+		cpyowner = malloc(len_owner + 1);
+	
+	if (cpyowner == NULL)
 		return (NULL);
-											}
-	for (i = 0; i <= strlen(owner); i++)
-	{
-		dog->owner[i] = owner[i];
-		return (dog);
-	}
+	for (i = 0; owner[i]; i++)
+		cpyowner[i] = owner[i];
+		cpyowner[i] = '\0';
+		new_dog->name = cpyname;
+		new_dog->age = age;
+		new_dog->owner = cpyowner;
+		return (new_dog);
 }
