2ec2cd3292257224efe1cb9828032dd27382843e
Profjay <jibstechnology@gmail.com>
2022-08-03 15:42:59 -0700

    All tasks commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: zero if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int num_bytes = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		
+		if (i < num_bytes - 1)
+		{
+				printf(" ");
+		}
+		else
+		{
+				printf("\n");
+		}
+	}
+	return (0);
+}
