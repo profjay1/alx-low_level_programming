be2bc92709724144e0b20cc33d656847ad050f25
Profjay <jibstechnology@gmail.com>
2022-08-03 16:58:48 -0700

    main.c commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,44 @@
 #include "3-calc.h"
 
 /**
- * main - performs simpe operation
+ * main - check the code for Holberton School students.
  *
- * @argc: argument count
+ * @argc: argument count.
  *
- * @argv: argument vector
+ * @argv: argument vector.
  *
- * Return: 0 if success
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int a = 0;
-	int b = 0;
+	int a, b;
 
-	int (*func)(int, int);
-
-	int res = 0;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	func = get_op_func(argv[2]);
 
-	if (func == NULL)
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	res = func(a, b);
-	printf("%d\n", res);
 
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	
+	printf("%d\n", operation(a, b));
 	return (0);
 }
