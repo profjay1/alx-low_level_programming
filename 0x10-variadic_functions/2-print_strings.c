3065403324e19a9ab0294b3f8ab3c9cb0e11e14e
Profjay <jibstechnology@gmail.com>
2022-08-04 07:50:17 -0700

    Print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,9 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	if (n > 0)
 	{
-		string = va_arg(args, char *);
-		
-		if (string == NULL)
+		string = va_arg(args, char f(string == NULL)
 		{
 			string = "(nil)";
 		}
