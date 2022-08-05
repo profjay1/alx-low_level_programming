4e92509d22f156b43e9e3b69ec4435980da8f566
Profjay <jibstechnology@gmail.com>
2022-08-05 15:29:31 -0700

    get op_func commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,11 +1,14 @@
 #include "3-calc.h"
 
+
 /**
- * get_op_func - select the correct function to do the callback
+ * get_op_func - selects the correct function to perform
  *
- * @s: operator to be compared
+ * the operation asked by the user.
  *
- * Return: pointer to the correct function
+ * @s: char operator.
+ *
+ * Return: pointer to the function that corresponds to the operator.
  */
 
 int (*get_op_func(char *s))(int, int)
@@ -21,13 +24,11 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (i < 10)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-		{
-			return (ops[i].f);
-		}
+		if (s[0] == ops->op[i])
+			break;
 		i++;
 	}
-	return (NULL);
+	return (ops[i / 2].f);
 }
