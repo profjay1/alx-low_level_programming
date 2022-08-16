2cb3be0329dc3b6d1e1808b1775783bff69bb7c9
Profjay <jibstechnology@gmail.com>
2022-08-16 14:52:01 -0700

    Print list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,47 +1,26 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a list even with loop
- * @head: pointer to head
+ * print_listint_safe - function that prints a linked list with a loop
+ * @head: head of the linked list
  *
- * Return: number of nodes
+ * Return: number of nodes of the linked list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int count = 0;
-	const listint_t *visited[1024];
-
-	while (head)
+	if (head)
 	{
-		if (is_visited(head, visited, count))
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			break;
-		}
-		visited[count++] = head;
 		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-	}
-	return (count);
-}
-
-/**
- * is_visited - check if a node is visited
- * @node: pointer to node
- * @visited: list of visited
- * @count: length of visited
- *
- * Return: 1 if is visited and 0 otherwise
- */
-int is_visited(const listint_t *node, const listint_t **visited, int count)
-{
-	int i = 0;
-
-	while (i < count)
-	{
-		if (node == visited[i])
+		if (head->next < head)
+		{
+			return (1 + print_listint_safe(head->next));
+		}
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			return (1);
-		i++;
+		}
+
 	}
 	return (0);
 }
