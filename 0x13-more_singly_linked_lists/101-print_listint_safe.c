9248b4aa154e38ee8412274eaf311b362bddf302
Profjay <jibstechnology@gmail.com>
2022-08-16 14:49:23 -0700

    Print list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,69 +1,47 @@
 #include "lists.h"
 
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
+ * print_listint_safe - prints a list even with loop
+ * @head: pointer to head
  *
- * Return: no return.
- */
-void free_listp(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * print_listint_safe - prints a linked list.
- * @head: head of a list.
- *
- * Return: number of nodes in the list.
+ * Return: number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
+	int count = 0;
+	const listint_t *visited[1024];
 
-	hptr = NULL;
-	while (head != NULL)
+	while (head)
 	{
-		new = malloc(sizeof(listp_t));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)head;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
+		if (is_visited(head, visited, count))
 		{
-			add = add->next;
-			if (head == add->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listp(&hptr);
-				return (nnodes);
-			}
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
 		}
-
+		visited[count++] = head;
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
-		nnodes++;
 	}
+	return (count);
+}
 
-	free_listp(&hptr);
-	return (nnodes);
+/**
+ * is_visited - check if a node is visited
+ * @node: pointer to node
+ * @visited: list of visited
+ * @count: length of visited
+ *
+ * Return: 1 if is visited and 0 otherwise
+ */
+int is_visited(const listint_t *node, const listint_t **visited, int count)
+{
+	int i = 0;
+
+	while (i < count)
+	{
+		if (node == visited[i])
+			return (1);
+		i++;
+	}
+	return (0);
 }
