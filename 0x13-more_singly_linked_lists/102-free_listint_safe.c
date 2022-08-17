6521d242d06686ca172254961ffc1a9106071d20
Profjay <jibstechnology@gmail.com>
2022-08-17 15:08:31 -0700

    102 correction

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,52 @@
 #include "lists.h"
 
+int is_visited2(listint_t *node, listint_t **visited, int count);
+
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
+ * free_listint_safe - prints a list even with loop
+ * @head: pointer to head
  *
- * Return: number of elements in the freed list
+ * Return: number of nodes
  */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+	listint_t *tmp, *visited[1024];
+	int count = 0;
 
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
+	if (!head)
+		return (-1);
+	while (*head)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
+		if (is_visited2(*head, visited, count))
 		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			len++;
+			*head = NULL;
 			break;
 		}
+		visited[count++] = *head;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
+	return (count);
+}
 
-	*h = NULL;
+/**
+ * is_visited2 - check if a node is visited
+ * @node: pointer to node
+ * @visited: list of visited
+ * @count: length of visited
+ *
+ * Return: 1 if is visited and 0 otherwise
+ */
+int is_visited2(listint_t *node, listint_t **visited, int count)
+{
+	int i = 0;
 
-	return (len);
+	while (i < count)
+	{
+		if (node == visited[i])
+			return (1);
+		i++;
+	}
+	return (0);
 }
